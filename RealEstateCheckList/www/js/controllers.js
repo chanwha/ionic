d5fc92b4c66ec3e47c759466d468579a8caf5b9e
chanwha <hong.chan.wha@gmail.com>
2015-10-25 12:37:34 +0900

    실패

--- RealEstateCheckList/www/js/controllers.js
+++ RealEstateCheckList/www/js/controllers.js
@@ -57,7 +57,7 @@ angular.module('app.controllers', [])
 	
 })
    
-.controller('addGroupCtrl', function($scope, $ionicPopup, $state, groupFactory) {
+.controller('addGroupCtrl', function($scope, $ionicPopup, $state, groupFactory, $cordovaCamera) {
      
       $scope.list = groupFactory.all();
 	  var d = new Date(); 
@@ -121,8 +121,37 @@ angular.module('app.controllers', [])
 		$state.go('groupListApp');
       };
     
-    
-    		var takePicture = function(){
+	$scope.takePicture = function(){
+		document.addEventListener("deviceready", function () {
+
+			var options = {
+			  quality: 50,
+			  destinationType: Camera.DestinationType.DATA_URL,
+			  sourceType: Camera.PictureSourceType.CAMERA,
+			  allowEdit: true,
+			  encodingType: Camera.EncodingType.JPEG,
+			  targetWidth: 100,
+			  targetHeight: 100,
+			  popoverOptions: CameraPopoverOptions,
+			  saveToPhotoAlbum: false,
+			  correctOrientation:true
+			};
+			
+			
+			$cordovaCamera.getPicture(options).then(function(imageData) {
+			  var image = document.getElementById('myImage');
+			  image.src = "data:image/jpeg;base64," + imageData;
+			}, function(err) {
+			  // errorA
+			});
+	
+
+		}, false);
+	};
+  
+    /*
+    		//var takePicture = function(){
+			$scope.takePicture = function(){
 			var options = {     
 				quality          : 75,
 				destinationType  : Camera.DestinationType.DATA_URL,
@@ -131,17 +160,19 @@ angular.module('app.controllers', [])
 				encodingType     : Camera.EncodingType.JPEG,
 				targetWidth      : 300,
 				targetHeight     : 300,
-				popoverOptions   : CameraPopoverOptions,
+					popoverOptions   : CameraPopoverOptions,
 				saveToPhotoAlbum : false
 			};
-			navigator.camera.getPicture(function(imageURI) {
+			
+			$cordovaCamera.getPicture(options)
+			//navigator.camera.getPicture(function(imageURI) {
+//
+			//}, function(err) {
 
-			}, function(err) {
-
-			}, options);
+			//}, options);
 		};
-		$scope.takePicture = takePicture;
-
+		//$scope.takePicture = takePicture;
+	*/
 	
 
 })
